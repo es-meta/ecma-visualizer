2aab362fa31c4a8db3be4b15f1f5bb9ad67e48da
Minseok Choe <choems@outlook.com>
2025-05-09 23:01:09 +0900

    remove: unused

--- utils/message.utils.ts
+++
@@ -1,9 +0,0 @@
-import { Message } from "@/types/message";
-
-export function fromRuntime(runtime: never): void {
-  // return {
-  //   from: runtime.from,
-  //   targetWindowId: runtime.targetWindowId,
-  //   payload: runtime.payload,
-  // };
-}
